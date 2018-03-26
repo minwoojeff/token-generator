b6ed906ab40f9452516668cea61d9472884b45d3
Minwoo Yoon <minwoo.jeff@gmail.com>
2018-03-25 18:52:52 -0700

    Health check and Grant Type handling

--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"crypto/subtle"
 	"encoding/json"
-	"io/ioutil"
+	"errors"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -39,45 +40,70 @@ func About(w http.ResponseWriter, r *http.Request) {
 	return
 }
 
-func Token(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotImplemented)
+func Health(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	return
+}
+
+func PasswordGrantToken(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("password grant here~"))
 	return
 }
 
 func TokenHandler(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-	AuthHandler(w, r, Token)
+	// Parse Body
+	var data map[string]string
+	err := json.NewDecoder(r.Body).Decode(&data)
+	if err != nil && err == io.EOF {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Missing required params"))
+		return
+	} else if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	// Auth
+	status, err := Authenticate(r, data)
+	if err != nil {
+		w.WriteHeader(status)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	switch grantType := data["grant_type"]; grantType {
+	case "password":
+		PasswordGrantToken(w, r)
+	case "authorization_code":
+		w.WriteHeader(http.StatusNotImplemented)
+		return
+	case "client_credentials":
+		w.WriteHeader(http.StatusNotImplemented)
+		return
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Unsupported grant type"))
+		return
+	}
 }
 
-func AuthHandler(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+func Authenticate(r *http.Request, data map[string]string) (int, error) {
 	user, pass, ok := r.BasicAuth()
-	// client_id and client_secert may be passed as part of payload
+	r.Close = false
+	// client_id and client_secret may be passed as part of payload
 	if !ok {
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte(err.Error()))
-			return
-		}
-
-		var data map[string]string
-		err = json.Unmarshal(body, &data)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte(err.Error()))
-			return
-		}
-
 		user = data["client_id"]
 		pass = data["client_secret"]
 	}
 
 	if subtle.ConstantTimeCompare([]byte(user), []byte(clientID)) != 1 || subtle.ConstantTimeCompare([]byte(pass), []byte(clientSecret)) != 1 {
-		w.WriteHeader(http.StatusUnauthorized)
-		w.Write([]byte("Unauthorized"))
-	} else {
-		log.Println("Authorized request.")
-		next(w, r)
+		return http.StatusUnauthorized, errors.New("Unauthorized")
 	}
+
+	log.Println("Successfully authenticated")
+	return 0, nil
 }
 
 func main() {
@@ -91,6 +117,8 @@ func main() {
 
 	// GET /, No Auth
 	main.HandleFunc("/", About).Methods("GET")
+	// GET /health, No Auth
+	main.HandleFunc("/health", Health).Methods("GET")
 
 	// POST /oauth/token, Authenticated
 	main.PathPrefix("/oauth/token").Handler(n).Methods("POST")
